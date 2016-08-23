12a5fb0f139ce97127b6635715a933f7a1f1ccb9
raphaelbischof <raphael.bischof@gmail.com>
2016-08-23 11:10:24 +0200

    feat: Handling trigger button position change

--- app/src/main/java/com/inandwin/halfmodalpresenterdemo/MainActivity.java
+++ app/src/main/java/com/inandwin/halfmodalpresenterdemo/MainActivity.java
@@ -1,19 +1,26 @@
 package com.inandwin.halfmodalpresenterdemo;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 
 import com.inandwin.halfmodalpresenter.HalfModalView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HalfModalView hfm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HalfModalView hfm = (HalfModalView) findViewById(R.id.halfModal);
+        hfm = (HalfModalView) findViewById(R.id.halfModal);
         hfm.setBackgroundButtonTrigger(ContextCompat.getDrawable(this, R.color.other));
         hfm.setBackgroundButtonExpanded(ContextCompat.getDrawable(this, R.color.other));
+    }
+
+    public void click(View v) {
+        hfm.setPositionFromTop(0.5f);
     }
 }
